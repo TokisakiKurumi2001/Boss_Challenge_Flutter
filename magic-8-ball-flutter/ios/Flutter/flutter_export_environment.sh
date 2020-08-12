16cd21d16ffe7283becf77d3d8fe96b8217dc396
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-08-12 17:09:40 +0700

    Fixing minor errors

--- magic-8-ball-flutter/ios/Flutter/flutter_export_environment.sh
+++ magic-8-ball-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/leminhkhoi/Works/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/leminhkhoi/programmer/Dart/flutter/magic-8-ball-flutter"
-export "FLUTTER_TARGET=/Users/leminhkhoi/programmer/Dart/flutter/magic-8-ball-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/leminhkhoi/programmer/Dart/flutter/Boss_Challenge_Flutter/magic-8-ball-flutter"
+export "FLUTTER_TARGET=/Users/leminhkhoi/programmer/Dart/flutter/Boss_Challenge_Flutter/magic-8-ball-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
